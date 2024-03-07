b65088c5e4ceb0c6709b60910b88dd682265695a
ideario-dev <ideario@naver.com>
2024-03-07 18:47:52 +0900

    Second day

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,43 @@
-export const trending = (req, res) => res.send("Home page Videos");
-export const see = (req, res) => res.send("watch!");
-export const edit = (req, res) => res.send("edit! Video!");
+const fakeUser = {
+  userName: "Nicolas",
+  loginIn: false,
+};
+
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 month ago",
+      views: 34,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 month ago",
+      views: 34,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 month ago",
+      views: 34,
+      id: 1,
+    },
+  ];
+  res.render("home", { pageTitle: "home", fakeUser, videos });
+};
+
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("search!");
 export const upload = (req, res) => res.send("upload");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const deleteVideo = (req, res) => {
+  console.log(req.params);
+  res.send("Delete Video");
+};
