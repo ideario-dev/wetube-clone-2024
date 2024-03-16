977830358f0ebe3c229bb19f8a4a405706ce39c4
ideario-dev <ideario@naver.com>
2024-03-17 08:43:23 +0900

    fifth day

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,24 @@
+import e from "express";
+
 export const localMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "WeTube";
-  res.locals.loggedInUser = req.session.user;
-  console.log(req.session);
+  res.locals.loggedInUser = req.session.user || {};
   next();
+};
+
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    next();
+  } else {
+    return res.redirect("/");
+  }
 };
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,21 @@
 import express from "express";
-import { edit, remove, logout, see } from "../controllers/userController.js";
+import {
+  getEdit,
+  postEdit,
+  remove,
+  logout,
+  see,
+  startGithubLogin,
+  finishGithubLogin,
+} from "../controllers/userController.js";
+import { protectorMiddleware, publicOnlyMiddleware } from "../middleware.js";
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
-userRouter.get("/remove", remove);
+userRouter.get("/logout", protectorMiddleware, logout);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get("/:id", see);
 
 export default userRouter;
